1866db70f7450918670965df9693c836c65bf462
Eamonn de Leastar <edeleastar@wit.ie>
2020-01-20 09:03:00 +0000

    introduce public folder for assets + extend routes to incliude

--- routes.js
+++ routes.js
@@ -1,3 +1,14 @@
 const Donations = require('./app/controllers/donations');
 
-module.exports = [{ method: 'GET', path: '/', config: Donations.index }];
+module.exports = [
+  { method: 'GET', path: '/', config: Donations.index },
+  {
+    method: 'GET',
+    path: '/{param*}',
+    handler: {
+      directory: {
+        path: './public'
+      }
+    }
+  }
+];
