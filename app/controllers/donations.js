54dae8f0d92b5d3f2e9b18f20760bbe65bad7f9c
Eamonn de Leastar <edeleastar@wit.ie>
2020-01-24 09:38:08 +0000

    revised routes + donation controller

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -1,7 +1,19 @@
+'use strict';
+
 const Donations = {
   index: {
     handler: function(request, h) {
       return h.file('./app/views/main.html');
+    }
+  },
+  signup: {
+    handler: function(request, h) {
+      return h.file('./app/views/signup.html');
+    }
+  },
+  login: {
+    handler: function(request, h) {
+      return h.file('./app/views/login.html');
     }
   }
 };
--- routes.js
+++ routes.js
@@ -2,6 +2,8 @@ const Donations = require('./app/controllers/donations');
 
 module.exports = [
   { method: 'GET', path: '/', config: Donations.index },
+  { method: 'GET', path: '/signup', config: Donations.signup },
+  { method: 'GET', path: '/login', config: Donations.login },
   {
     method: 'GET',
     path: '/{param*}',
