b0db4f9002e5164668c96b929895612af6fd67e5
Eamonn de Leastar <edeleastar@wit.ie>
2020-01-28 09:51:35 +0000

    initialise and retrieve cooke in login/signup

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -2,40 +2,47 @@
 
 const Accounts = {
   index: {
+    auth: false,
     handler: function(request, h) {
       return h.view('main', { title: 'Welcome to Donations' });
     }
   },
   showSignup: {
+    auth: false,
     handler: function(request, h) {
       return h.view('signup', { title: 'Sign up for Donations' });
     }
   },
   signup: {
+    auth: false,
     handler: function(request, h) {
       const user = request.payload;
       this.users[user.email] = user;
-      this.currentUser = user;
+      request.cookieAuth.set({ id: user.email });
       return h.redirect('/home');
     }
   },
   showLogin: {
+    auth: false,
     handler: function(request, h) {
       return h.view('login', { title: 'Login to Donations' });
     }
   },
   login: {
+    auth: false,
     handler: function(request, h) {
       const user = request.payload;
       if (user.email in this.users && user.password === this.users[user.email].password) {
-        this.currentUser = this.users[user.email];
+        request.cookieAuth.set({ id: user.email });
         return h.redirect('/home');
       }
       return h.redirect('/');
     }
   },
   logout: {
+    auth: false,
     handler: function(request, h) {
+      request.cookieAuth.clear();
       return h.redirect('/');
     }
   }
