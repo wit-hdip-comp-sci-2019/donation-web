9c4ad02786468c1ccbf0e9f8c216050dae3125ae
Eamonn de Leastar <edeleastar@wit.ie>
2020-01-31 16:58:53 +0000

    read cookie details from .env

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('@hapi/hapi');
+require('dotenv').config();
 
 const server = Hapi.server({
   port: 3000,
@@ -19,8 +20,8 @@ async function init() {
 
   server.auth.strategy('session', 'cookie', {
     cookie: {
-      name: 'donation',
-      password: 'password-should-be-32-characters',
+      name: process.env.cookie_name,
+      password: process.env.cookie_password,
       isSecure: false
     },
     redirectTo: '/'
