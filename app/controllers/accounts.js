dfb027c78c6746e48126dad953a452918436763c
Eamonn de Leastar <edeleastar@wit.ie>
2020-01-31 18:25:42 +0000

    detect and report errors on login

--- app/controllers/accounts.js
+++ app/controllers/accounts.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const User = require('../models/user');
+const Boom = require('@hapi/boom');
 
 const Accounts = {
   index: {
@@ -40,15 +41,18 @@ const Accounts = {
     auth: false,
     handler: async function(request, h) {
       const { email, password } = request.payload;
-      let user = await User.findByEmail(email);
-      if (!user) {
-        return h.redirect('/');
-      }
-      if (user.comparePassword(password)) {
+      try {
+        let user = await User.findByEmail(email);
+        if (!user) {
+          const message = 'Email address is not registered';
+          throw Boom.unauthorized(message);
+        }
+        user.comparePassword(password);
         request.cookieAuth.set({ id: user.id });
         return h.redirect('/home');
+      } catch (err) {
+        return h.view('login', { errors: [{ message: err.message }] });
       }
-      return h.redirect('/');
     }
   },
   logout: {
