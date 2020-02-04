7ef759914f2e8f1e680ff7e2c470232fe679f4dc
Eamonn de Leastar <edeleastar@wit.ie>
2020-02-04 10:21:28 +0000

    error strategy in donation + main include err

--- app/controllers/donations.js
+++ app/controllers/donations.js
@@ -11,25 +11,33 @@ const Donations = {
   },
   report: {
     handler: async function(request, h) {
-      const donations = await Donation.find().populate('donor').lean();
-      return h.view('report', {
-        title: 'Donations to Date',
-        donations: donations
-      });
+      try {
+        const donations = await Donation.find().populate('donor').lean();
+        return h.view('report', {
+          title: 'Donations to Date',
+          donations: donations
+        });
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
     }
   },
   donate: {
     handler: async function(request, h) {
-      const id = request.auth.credentials.id;
-      const user = await User.findById(id);
-      const data = request.payload;
-      const newDonation = new Donation({
-        amount: data.amount,
-        method: data.method,
-        donor: user._id
-      });
-      await newDonation.save();
-      return h.redirect('/report');
+      try {
+        const id = request.auth.credentials.id;
+        const user = await User.findById(id);
+        const data = request.payload;
+        const newDonation = new Donation({
+          amount: data.amount,
+          method: data.method,
+          donor: user._id
+        });
+        await newDonation.save();
+        return h.redirect('/report');
+      } catch (err) {
+        return h.view('main', { errors: [{ message: err.message }] });
+      }
     }
   }
 };
