197028cb4b55cb3f3df5a53848ae09c7d6258fde
Eamonn de Leastar <edeleastar@wit.ie>
2020-03-09 09:28:03 +0000

    09.exercises

--- test/candidate-api-test.js
+++ test/candidate-api-test.js
@@ -3,9 +3,8 @@
 const assert = require('chai').assert;
 const axios = require('axios');
 
-suite('Candidate API tests', function () {
-
-  test('get candidates', async function () {
+suite('Candidate API tests', function() {
+  test('get candidates', async function() {
     const response = await axios.get('http://localhost:3000/api/candidates');
     const candidates = response.data;
     assert.equal(2, candidates.length);
@@ -19,7 +18,7 @@ suite('Candidate API tests', function () {
     assert.equal(candidates[1].office, 'President');
   });
 
-  test('get one candidate', async function () {
+  test('get one candidate', async function() {
     let response = await axios.get('http://localhost:3000/api/candidates');
     const candidates = response.data;
     assert.equal(2, candidates.length);
@@ -33,12 +32,12 @@ suite('Candidate API tests', function () {
     assert.equal(oneCandidate.office, 'President');
   });
 
-  test('create a candidate', async function () {
+  test('create a candidate', async function() {
     const candidatesUrl = 'http://localhost:3000/api/candidates';
     const newCandidate = {
       firstName: 'Barnie',
       lastName: 'Grumble',
-      office: 'President',
+      office: 'President'
     };
 
     const response = await axios.post(candidatesUrl, newCandidate);
@@ -48,5 +47,34 @@ suite('Candidate API tests', function () {
     assert.equal(returnedCandidate.firstName, 'Barnie');
     assert.equal(returnedCandidate.lastName, 'Grumble');
     assert.equal(returnedCandidate.office, 'President');
+  });
+
+  test('delete a candidate', async function() {
+    let response = await axios.get('http://localhost:3000/api/candidates');
+    let candidates = response.data;
+    const originalSize = candidates.length;
+
+    const oneCandidateUrl = 'http://localhost:3000/api/candidates/' + candidates[0]._id;
+    response = await axios.get(oneCandidateUrl);
+    const oneCandidate = response.data;
+    assert.equal(oneCandidate.firstName, 'Lisa');
+
+    response = await axios.delete('http://localhost:3000/api/candidates/' + candidates[0]._id);
+    assert.equal(response.data.success, true);
+
+    response = await axios.get('http://localhost:3000/api/candidates');
+    candidates = response.data;
+    assert.equal(candidates.length, originalSize - 1);
+  });
+
+  test('delete all candidates', async function() {
+    let response = await axios.get('http://localhost:3000/api/candidates');
+    let candidates = response.data;
+    const originalSize = candidates.length;
+    assert(originalSize > 0);
+    response = await axios.delete('http://localhost:3000/api/candidates');
+    response = await axios.get('http://localhost:3000/api/candidates');
+    candidates = response.data;
+    assert.equal(candidates.length, 0);
   });
 });
