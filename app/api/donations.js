ced3e41a11a28b9b259d062abaf52a7e257cc982
Eamonn de Leastar <edeleastar@wit.ie>
2020-03-26 07:38:50 +0000

    introduce initial donations api implementation

---
+++ app/api/donations.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const Donation = require('../models/donation');
+const Boom = require('@hapi/boom');
+
+const Donations = {
+  findAll: {
+    auth: false,
+    handler: async function (request, h) {
+      const donations = await Donation.find();
+      return donations;
+    }
+  }
+};
+
+module.exports = Donations;
--- routes-api.js
+++ routes-api.js
@@ -1,5 +1,6 @@
 const Candidates = require('./app/api/candidates');
 const Users = require('./app/api/users');
+const Donations = require('./app/api/donations');
 
 module.exports = [
   { method: 'GET', path: '/api/candidates', config: Candidates.find },
@@ -12,5 +13,7 @@ module.exports = [
   { method: 'GET', path: '/api/users/{id}', config: Users.findOne },
   { method: 'POST', path: '/api/users', config: Users.create },
   { method: 'DELETE', path: '/api/users/{id}', config: Users.deleteOne },
-  { method: 'DELETE', path: '/api/users', config: Users.deleteAll }
+  { method: 'DELETE', path: '/api/users', config: Users.deleteAll },
+
+  { method: 'GET', path: '/api/donations', config: Donations.findAll }
 ];
