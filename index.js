017d3d1d50a2e4e2f5152c4963c8a7d8d23d4ccc
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-27 18:48:43 +0100

    register and initialize jwt plugin

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 const Hapi = require('@hapi/hapi');
 const dotenv = require('dotenv');
+const utils = require('./app/api/utils.js');
 
 const result = dotenv.config();
 if (result.error) {
@@ -20,6 +21,7 @@ async function init() {
   await server.register(require('@hapi/inert'));
   await server.register(require('@hapi/vision'));
   await server.register(require('@hapi/cookie'));
+  await server.register(require('hapi-auth-jwt2'));
 
   server.validator(require('@hapi/joi'))
 
@@ -30,6 +32,12 @@ async function init() {
       isSecure: false
     },
     redirectTo: '/'
+  });
+
+  server.auth.strategy('jwt', 'jwt', {
+    key: 'secretpasswordnotrevealedtoanyone',
+    validate: utils.validate,
+    verifyOptions: { algorithms: ['HS256'] },
   });
 
   server.auth.default('session');
