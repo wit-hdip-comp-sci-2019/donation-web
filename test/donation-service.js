8ecb8eaa97d98f8bcff4ef6b488071eca2fb2f48
Eamonn de Leastar <edeleastar@wit.ie>
2020-04-27 18:54:23 +0100

    extend donation-service to support authentication

--- test/donation-service.js
+++ test/donation-service.js
@@ -127,10 +127,15 @@ class DonationService {
   async authenticate(user) {
     try {
       const response = await axios.post(this.baseUrl + '/api/users/authenticate', user);
+      axios.defaults.headers.common['Authorization'] = 'Bearer ' + response.data.token;
       return response.data;
     } catch (e) {
       return null;
     }
+  }
+
+  async clearAuth(user) {
+    axios.defaults.headers.common['Authorization'] = '';
   }
 }
 
