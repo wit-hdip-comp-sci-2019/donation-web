ce6352e0aecd2051ed79f4cd9070aaa91f3744e2
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-04 16:54:21 +0000

    include authentication route

--- app/api/users.js
+++ app/api/users.js
@@ -57,6 +57,24 @@ const Users = {
       return Boom.notFound('id not found');
     },
   },
+
+  authenticate: {
+    auth: false,
+    handler: async function (request, h) {
+      try {
+        const user = await User.findOne({ email: request.payload.email });
+        if (!user) {
+          return Boom.unauthorized('User not found');
+        } else if (user.password !== request.payload.password) {
+          return Boom.unauthorized('Invalid password');
+        } else {
+          return user;
+        }
+      } catch (err) {
+        return Boom.notFound('internal db failure');
+      }
+    },
+  },
 };
 
 module.exports = Users;
