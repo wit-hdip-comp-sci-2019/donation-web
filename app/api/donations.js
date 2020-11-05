4d185edba5cefdeac9a768c6850fb08908c25797
Eamonn de Leastar <edeleastar@wit.ie>
2020-11-05 08:31:54 +0000

    formatting updates

--- app/api/donations.js
+++ app/api/donations.js
@@ -7,21 +7,21 @@ const Candidate = require('../models/candidate');
 const Donations = {
   findAll: {
     auth: false,
-    handler: async function(request, h) {
+    handler: async function (request, h) {
       const donations = await Donation.find();
       return donations;
-    }
+    },
   },
   findByCandidate: {
     auth: false,
-    handler: async function(request, h) {
+    handler: async function (request, h) {
       const donations = await Donation.find({ candidate: request.params.id });
       return donations;
-    }
+    },
   },
   makeDonation: {
     auth: false,
-    handler: async function(request, h) {
+    handler: async function (request, h) {
       let donation = new Donation(request.payload);
       const candidate = await Candidate.findOne({ _id: request.params.id });
       if (!candidate) {
@@ -30,15 +30,15 @@ const Donations = {
       donation.candidate = candidate._id;
       donation = await donation.save();
       return donation;
-    }
+    },
   },
   deleteAll: {
     auth: false,
-    handler: async function(request, h) {
+    handler: async function (request, h) {
       await Donation.deleteMany({});
       return { success: true };
-    }
-  }
+    },
+  },
 };
 
 module.exports = Donations;
